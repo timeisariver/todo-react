56501eeaa3ba561e851e1d7907acc8b4f4c3425d
timeisariver <bdthesun@gmail.com>
2026-09-07 01:30:14 +0900

    完了タスクを表示

--- app/components/TodoList/TodoList.js
+++ app/components/TodoList/TodoList.js
@@ -1,11 +1,18 @@
 import TodoRow from "@/app/components/TodoRow/TodoRow";
 import styles from "./TodoList.module.scss";
+import { useState } from "react";
 
 export default function TodoList({ tasks, onSubmit }) {
+  const [showCompleted, setShowCompleted] = useState(false);
+
   return (
     <div className={styles.content}>
       <label className={styles.showCompleted}>
-        <input type="checkbox" defaultChecked />
+        <input
+          type="checkbox"
+          checked={showCompleted}
+          onChange={(e) => setShowCompleted(e.target.checked)}
+        />
         <p>完了タスクを表示</p>
       </label>
       <div className={styles.head}>
@@ -17,16 +24,20 @@ export default function TodoList({ tasks, onSubmit }) {
         <div className={styles.headItem}>削除</div>
       </div>
       <div className={styles.list}>
-        {tasks.map((task) => (
-          <TodoRow
-            onSubmit={onSubmit}
-            key={task.id}
-            id={task.id}
-            name={task.name}
-            deadline={task.deadline}
-            completed={task.completed}
-          />
-        ))}
+        {tasks
+          .filter((task) => showCompleted || !task.completed)
+          .map((task) => {
+            return (
+              <TodoRow
+                onSubmit={onSubmit}
+                key={task.id}
+                id={task.id}
+                name={task.name}
+                deadline={task.deadline}
+                completed={task.completed}
+              />
+            );
+          })}
       </div>
     </div>
   );
