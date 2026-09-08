72e5c04ad6ae84996e673839bb35544b6a5df578
timeisariver <bdthesun@gmail.com>
2026-09-08 19:29:56 +0900

    GitHub Pages 用に basePath と assetPrefix を設定

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   output: "export",
+
+  basePath: isProd ? "/todo-react" : "",
+  assetPrefix: isProd ? "/todo-react" : "",
 
   images: {
     unoptimized: true,
